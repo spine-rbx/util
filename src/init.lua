d6173a43d8ba2870e1d77fe4ec2ec4afaf298ab1
Uncontained0 <uncontained0@gmail.com>
2022-06-20 19:36:55 -0500

    init

---
+++ src/init.lua
@@ -0,0 +1,37 @@
+local Util = {}
+
+function Util.Make(Class, Props: { [string]: any }?, Children: { Instance }?)
+	local Object = Instance.new(Class)
+
+	if type(Props) == "table" then
+		for i, v in pairs(Props) do
+			Object[i] = v
+		end
+	end
+
+	if type(Children) == "table" then
+		for _, v in ipairs(Children) do
+			v.Parent = Object
+		end
+	end
+
+	return Object
+end
+
+function Util.Each(Parent: Instance, ChildAdded: (Child: Instance) -> ()?, ChildRemoved: (Child: Instance) -> ()?)
+	local Connections = {}
+
+	if ChildAdded then
+		Connections.ChildAdded = Parent.ChildAdded:Connect(ChildAdded)
+
+		for _, v in ipairs(Parent:GetChildren()) do
+			task.spawn(ChildAdded, v)
+		end
+	end
+
+	if ChildRemoved then
+		Connections.ChildRemoved = Parent.ChildRemoved:Connect(ChildRemoved)
+	end
+end
+
+return Util
